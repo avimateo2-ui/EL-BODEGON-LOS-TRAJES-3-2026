3fb491fa61b8a32b43446c86e5ec3dcd383d1ddf
avimateo2@gmail.com <mateoceellphoe2@gmail.com>
2026-08-21 20:10:07 -0500

    vCard: boton visible para descargar contacto ELBODEGONDELOSTRAJES + boton flotante morado + instrucciones

--- js/app.js
+++ js/app.js
@@ -298,19 +298,18 @@
     }
 
     // vCard: descargar contacto con nombre ELBODEGONDELOSTRAJES
-    var contactLink = document.getElementById('download-contact');
-    if (contactLink) {
-      var vCard = [
-        'BEGIN:VCARD',
-        'VERSION:3.0',
-        'FN:ELBODEGONDELOSTRAJES',
-        'N:ELBODEGONDELOSTRAJES;;;;',
-        'ORG:El Bodegón de los Trajes',
-        'TEL;TYPE=CELL:+573107706615',
-        'ADR;TYPE=WORK:;;Tunja;Boyacá;;;;Colombia',
-        'URL:https://el-bodegon-los-trajes-3-2026.vercel.app',
-        'END:VCARD'
-      ].join('\r\n');
-      var blob = new Blob([vCard], { type: 'text/vcard;charset=utf-8' });
-      contactLink.href = URL.createObjectURL(blob);
-    }
+    var vCard = [
+      'BEGIN:VCARD',
+      'VERSION:3.0',
+      'FN:ELBODEGONDELOSTRAJES',
+      'N:ELBODEGONDELOSTRAJES;;;;',
+      'ORG:El Bodegón de los Trajes',
+      'TEL;TYPE=CELL:+573107706615',
+      'ADR;TYPE=WORK:;;Tunja;Boyacá;;;;Colombia',
+      'URL:https://el-bodegon-los-trajes-3-2026.vercel.app',
+      'END:VCARD'
+    ].join('\r\n');
+    var vCardBlob = new Blob([vCard], { type: 'text/vcard;charset=utf-8' });
+    var vCardUrl = URL.createObjectURL(vCardBlob);
+    var contactLinks = document.querySelectorAll('#download-contact, #float-download-contact');
+    contactLinks.forEach(function(link) { link.href = vCardUrl; });
