26fdbb54963287a97c71346980df1dd3913d55ec
El Bodegón Admin <avimateo2@gmail.com>
2026-08-26 13:35:32 -0500

    Admin update: contenido actualizado desde el panel

--- data/admin-content.js
+++ data/admin-content.js
@@ -1,1 +1,16 @@
-window.ADMIN_CONTENT = {"version":2,"texts":[],"images":[],"addCards":[],"addTexts":[],"deleteCards":[],"deleteTexts":[],"seasonCovers":{},"photoSettings":{},"editorStyles":{"test_debug":"color:red;"}};
+window.ADMIN_CONTENT = {
+  "version": 2,
+  "usernameHash": null,
+  "passwordHash": null,
+  "texts": [],
+  "images": [],
+  "addCards": [],
+  "addTexts": [],
+  "deleteCards": [],
+  "deleteTexts": [],
+  "seasonCovers": {},
+  "photoSettings": {},
+  "editorStyles": {
+    "e1787769276333_w636": "--d: 90ms; left: 0px; top: 0px;"
+  }
+};
