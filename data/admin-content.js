44772dfeef1fdcc83418b3a6705e9b474dee82f2
El Bodegón Admin <avimateo2@gmail.com>
2026-08-28 15:29:32 -0500

    Admin update: contenido actualizado desde el panel

--- data/admin-content.js
+++ data/admin-content.js
@@ -45,7 +45,7 @@ window.ADMIN_CONTENT = {
     }
   },
   "editorStyles": {
-    "e1787948874222_9mrm": "width: 1378px; height: 3238px;",
+    "e1787948874222_9mrm": "width: 1520px; height: 3566px;",
     "e1787948762885_f8vm": "",
     "e1787948938115_igpv": "",
     "e1787948823662_t6sx": "--d: 180ms; width: 1125px; height: 571px;",
